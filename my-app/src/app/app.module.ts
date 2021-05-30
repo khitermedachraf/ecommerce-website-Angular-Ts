5f11620db1980ff22140e5479062027ad3c8dec0
KHITER Med Achraf <khiter.med7@gmail.com>
2021-05-30 16:06:33 +0100

    add register-user interface(logIn & log Up)

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,25 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
+import  {MatIconModule}  from '@angular/material/icon';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+
+
+
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegisterUserComponent } from './register-user/register-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    RegisterUserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatTabsModule,
+    MatCardModule,
+    MatInputModule,
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
